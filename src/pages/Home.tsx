7ef8759cd558e50bf6d49055f2f503d05189c5c5
USER <sithiraroneth@gmail.com>
2025-04-06 21:47:47 +0530

    add home ui

---
+++ src/pages/Home.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom"; // Note: Use 'react-router-dom' for React Router v6
+
+export default function Home() {
+    return (
+        <div className="flex flex-col items-center justify-center mt-32 px-4">
+            <h1 className="md:text-4xl text-2xl font-bold text-center tracking-wide">
+                Healthy Cooking Recipes
+            </h1>
+            <h1 className="md:text-4xl text-2xl font-bold text-center tracking-wide mt-2">
+                and the Right Nutrition
+            </h1>
+            <p className="md:font-semibold md:mt-6 text-center mt-4 text-lg">
+                Browse through Over 50 Tasty Recipes
+            </p>
+            <Link to="/recipes">
+                <button className="px-11 py-2.5 bg-amber-900 text-white rounded-tl-3xl rounded-br-3xl mt-6 md:mt-8 text-lg">
+                    More Recipes
+                </button>
+            </Link>
+        </div>
+    );
+}
