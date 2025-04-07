bbe3b48454698237165234fcb98d993be74cc307
USER <sithiraroneth@gmail.com>
2025-04-07 11:46:33 +0530

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import './App.css';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import store from './store/Store.ts';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import RootLayout from './components/RootLayout.tsx';
 import Home from './pages/Home.tsx';
 import Recipes from './pages/Recipes.tsx';
-import { Navigate } from 'react-router-dom';
+import {Navigate} from 'react-router-dom';
+import Profile from "./pages/Profile.tsx";
+import RecipeDetail from "./components/RecipeDetails.tsx";
 
 function App() {
     const routes = createBrowserRouter([
         {
-            path: '/', element: <RootLayout />,
+            path: '/', element: <RootLayout/>,
             children: [
-                { path: '/', element: <Navigate to="/home" /> },
-                { path: '/home', element: <Home /> },
-                { path: '/recipes', element: <Recipes /> },
+                {path: '/', element: <Navigate to="/home"/>},
+                {path: '/home', element: <Home/>},
+                {path: '/recipes', element: <Recipes/>},
+                {path: '/profile', element: <Profile/>},
+                {path: '/recipe/:id', element: <RecipeDetail/>}
             ]
         }
     ]);
     return (
         <Provider store={store}>
-            <RouterProvider router={routes} />
+            <RouterProvider router={routes}/>
         </Provider>
     );
 }
