b24e82011afbf510a22003106a28ad01ab2c9bbc
USER <sithiraroneth@gmail.com>
2025-04-07 19:36:11 +0530

    add addRecipe, updateRecipe, deleteRecipe reducers

--- src/reducers/recipeSlice.ts
+++ src/reducers/recipeSlice.ts
@@ -1,12 +1,27 @@
-import {createSlice} from "@reduxjs/toolkit";
-import {Recipe} from "../model/Recipe.ts";
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Recipe } from '../model/Recipe'; // Ensure your Recipe interface is updated
 
 const initialState: Recipe[] = [];
 
 const recipeSlice = createSlice({
     name: 'recipes',
     initialState: initialState,
-    reducers: {},
+    reducers: {
+        addRecipe: (state, action: PayloadAction<Recipe>) => {
+            state.push(action.payload);
+        },
+        updateRecipe: (state, action: PayloadAction<Recipe>) => {
+            const index = state.findIndex((recipe) => recipe.id === action.payload.id);
+            if (index !== -1) {
+                state[index] = action.payload;
+            }
+        },
+        deleteRecipe: (state, action: PayloadAction<string>) => {
+            return state.filter((recipe) => recipe.id !== action.payload);
+        },
+    },
 });
+
+export const { addRecipe, updateRecipe, deleteRecipe } = recipeSlice.actions;
 
 export default recipeSlice.reducer;
