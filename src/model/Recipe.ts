ad04391c0889f1451daa6e7f5a965adf2fb00a23
USER <sithiraroneth@gmail.com>
2025-04-09 12:54:42 +0530

    add attributes to Recipe

--- src/model/Recipe.ts
+++ src/model/Recipe.ts
@@ -1,3 +1,11 @@
-export class Recipe {
-
+export interface Recipe {
+    id: string;
+    title: string;
+    cookingtime: string;
+    calories: string;
+    rating: string;
+    image: string;
+    ingredients: string;
+    instructions: string;
+    username: string;
 }
