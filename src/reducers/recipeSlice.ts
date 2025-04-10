993ce2464af4d90b3dc5232179749882b87f9bff
USER <sithiraroneth@gmail.com>
2025-04-10 13:05:11 +0530

    Implement full CRUD operations (Create, Read, Update, Delete) in recipeSlice with async thunks

--- src/reducers/recipeSlice.ts
+++ src/reducers/recipeSlice.ts
@@ -1,26 +1,38 @@
-import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {Recipe} from '../model/Recipe';
+import axios from "axios";
 
 const initialState: Recipe[] = [];
-
+const api = axios.create({
+    baseURL: 'https://67f29b43ec56ec1a36d3a01c.mockapi.io/recipes'
+});
+export const addRecipe = createAsyncThunk<Recipe, Recipe>(
+    'recipes/addRecipe',
+    async (recipeData: Recipe) => {
+        const response = await api.post<Recipe>('/', recipeData);
+        return response.data;
+    }
+);
+export const updateRecipe = createAsyncThunk(
+    'recipes/updateRecipe',
+    async (updatedRecipe: Recipe) => {
+        const response = await api.put(`/${updatedRecipe.id}`, updatedRecipe);
+        return response.data;
+    }
+);
+export const deleteRecipe = createAsyncThunk(
+    'recipes/deleteRecipe',
+    async (id: string) => {
+        await api.delete(`/${id}`);
+        return id; // Return the id for the deleted recipe
+    }
+);
 const recipeSlice = createSlice({
     name: 'recipes',
     initialState: initialState,
     reducers: {
-        addRecipe: (state, action: PayloadAction<Recipe>) => {
-            state.push(action.payload);
-        },
         setRecipes: (state, action) => {
             return action.payload;
-        },
-        updateRecipe: (state, action: PayloadAction<Recipe>) => {
-            const index = state.findIndex((recipe) => recipe.id === action.payload.id);
-            if (index !== -1) {
-                state[index] = action.payload;
-            }
-        },
-        deleteRecipe: (state, action: PayloadAction<string>) => {
-            return state.filter((recipe) => recipe.id !== action.payload);
         },
         addFavorite: (state, action: PayloadAction<Recipe>) => {
             state.push(action.payload);
@@ -29,8 +41,25 @@ const recipeSlice = createSlice({
             return state.filter((recipe) => recipe.id !== action.payload);
         },
     },
+    extraReducers: (builder) => {
+        builder
+            .addCase(addRecipe.fulfilled, (state, action) => {
+                state.push(action.payload);
+            })
+        builder
+            .addCase(updateRecipe.fulfilled, (state, action) => {
+                const index = state.findIndex((recipe) => recipe.id === action.payload.id);
+                if (index !== -1) {
+                    state[index] = action.payload;
+                }
+            });
+        builder
+            .addCase(deleteRecipe.fulfilled, (state, action) => {
+                return state.filter((recipe) => recipe.id !== action.payload);
+            });
+    }
 });
 
-export const {addRecipe, updateRecipe, deleteRecipe, setRecipes,addFavorite,removeFavorite} = recipeSlice.actions;
+export const {setRecipes, addFavorite, removeFavorite} = recipeSlice.actions;
 
 export default recipeSlice.reducer;
